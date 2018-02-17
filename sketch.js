89feb0ec07a0dfcfafe129439f97f02904a93d9b
jenniferb16 <36575496+jenniferb16@users.noreply.github.com>
2018-02-17 16:18:21 -0600

    add summury

--- sketch.js
+++ sketch.js
@@ -4,5 +4,17 @@ function setup(){
 
 
 function draw(){
+  //red background
+  background(190,174,212);
+  //ellipse with blue border, green inside
+  stroke(0,0,255);
+if(mouseX > 300){
+  fill(0,255,0);
+}
+else{
+  fill(0,0,255);
+}
+//thicker border
+  strokeWeight(5);
  ellipse(50,50,50,50);
 }
